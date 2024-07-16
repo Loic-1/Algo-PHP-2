305d3722e43b5c762b48e5b08718ddecf04c1809
Loic-1 <loic.vasile130@gmail.com>
2024-07-16 14:39:19 +0200

    exo 3

---
+++ exo3.php
@@ -0,0 +1,7 @@
+<h2>Exercice 3</h2>
+<p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir 
+dans un nouvel onglet (_blank)</p>
+<h2>Résultat</h2>
+<?php
+
+echo '<a href="https://elan-formation.fr/accueil" target="_blank">ELAN FORMATION</a>';
